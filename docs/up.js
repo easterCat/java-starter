94d9068089e2974251504d4185c95ab05a30c0da
lilin <lilin@itcom888.com>
2020-01-08 18:52:39 +0800

    java-starter快速提交=>2020-1-8 6:52:29 PM

--- docs/up.js
+++ docs/up.js
@@ -1,4 +1,4 @@
 const { execSync } = require("child_process");
 const stamp = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
-const command = `gitbook build && gitbook build ./ ./docs && git pull && git add . && git commit -m "commonjs快速提交=>${stamp}" && git push -u origin master`;
+const command = `gitbook build && gitbook build ./ ./docs && git pull && git add . && git commit -m "java-starter快速提交=>${stamp}" && git push -u origin master`;
 execSync(command);
