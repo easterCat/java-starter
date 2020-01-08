f7487c792e7a1e40b891597b6036dc68e91b7aa1
lilin <lilin@itcom888.com>
2020-01-08 16:34:44 +0800

    commonjs快速提交=>2020-1-8 4:34:36 PM

---
+++ _book/docs/up.js
@@ -0,0 +1,7 @@
+const { execSync } = require("child_process");
+const stamp = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
+const command = `git add . && git commit -m "commonjs快速提交=>${stamp}" && git push -u origin master`;
+execSync("gitbook build");
+execSync("gitbook build ./ ./docs");
+execSync("git pull");
+execSync(command);
